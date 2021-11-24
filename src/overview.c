bc60da1e942d9fd2354e2b46480172260a18471e
Milan Toth <milgra@milgra.com>
2021-11-24 23:24:39 +0100

    dropped gtk

--- src/overview.c
+++ src/overview.c
@@ -13,7 +13,6 @@
 #include <X11/extensions/XInput.h>
 #include <X11/extensions/XInput2.h>
 #include <getopt.h>
-#include <gtk/gtk.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,125 +32,9 @@ struct
   char  panel_visible;
 } zm = {0};
 
-GtkWidget* window = NULL;
-GtkWidget* image  = NULL;
-bm_t*      bm     = NULL;
+bm_t* bm = NULL;
 
-int f_show  = 0;
-int f_hide  = 0;
-int visible = 0;
-int resize  = 0;
-
-gboolean redraw(gpointer data)
-{
-  if (f_show)
-  {
-    char buff[100] = {0};
-
-    // get i3 workspaces
-
-    FILE* pipe   = popen("i3-msg -t get_workspaces", "r"); // CLOSE 0
-    char* wstree = cstr_new_cstring("{\"items\":");        // REL 0
-
-    while (fgets(buff, sizeof(buff), pipe) != NULL) wstree = cstr_append(wstree, buff);
-
-    wstree = cstr_append(wstree, "}");
-    pclose(pipe); // CLOSE 0
-
-    pipe       = popen("i3-msg -t get_tree", "r"); // CLOSE 0
-    char* tree = cstr_new_cstring("");             // REL 0
-
-    while (fgets(buff, sizeof(buff), pipe) != NULL) tree = cstr_append(tree, buff);
-
-    pclose(pipe); // CLOSE 0
-
-    vec_t* workspaces = VNEW(); // RET 0
-
-    analyzer_extract(wstree, tree, workspaces);
-
-    REL(wstree); // REL 1
-    REL(tree);
-
-    i3_workspace_t* ws  = workspaces->data[0];
-    i3_workspace_t* wsl = workspaces->data[workspaces->length - 1];
-
-    if (ws->width > 0 && ws->height > 0)
-    {
-      int gap  = 25;
-      int cols = 5;
-      int rows = (int)ceilf((float)wsl->number / 5.0);
-
-      int lay_wth = cols * (ws->width / 8) + (cols + 1) * gap;
-      int lay_hth = rows * (ws->height / 8) + (rows + 1) * gap;
-
-      // create texture bitmap
-
-      if (bm == NULL || bm->w != lay_wth || bm->h != lay_hth)
-      {
-        bm     = bm_new(lay_wth, lay_hth); // REL 0
-        resize = 1;
-      }
-
-      renderer_draw(bm, workspaces);
-
-      GdkPixbuf* pb = gdk_pixbuf_new_from_data(bm->data, GDK_COLORSPACE_RGB, TRUE, 8, bm->w, bm->h, bm->w * 4, NULL, NULL);
-
-      gtk_image_set_from_pixbuf((GtkImage*)image, pb);
-
-      g_object_unref(pb);
-    }
-
-    REL(workspaces);
-
-    if (!visible)
-    {
-      /* if (resize) */
-      /* { */
-      /*   resize = 0; */
-      /* } */
-      gtk_widget_show(window);
-
-      gint rwidth, rheight;
-
-      // get screen geom
-
-      GdkWindow* root = gtk_widget_get_root_window(GTK_WIDGET(window));
-
-      gdk_window_get_geometry(root, NULL, NULL, &rwidth, &rheight);
-
-      gtk_window_resize(GTK_WINDOW(window), bm->w, bm->h);
-
-      gtk_window_move(GTK_WINDOW(window), (rwidth - bm->w) / 2, (rheight - bm->h) / 2);
-
-      visible = 1;
-    }
-    f_show = 0;
-  }
-
-  if (f_hide)
-  {
-    if (visible)
-    {
-      gtk_widget_hide(window);
-      visible = 0;
-    }
-    f_hide = 0;
-  }
-
-  return TRUE;
-}
-
-void show()
-{
-  f_show = 1;
-  redraw(NULL);
-}
-
-void hide()
-{
-  f_hide = 1;
-  redraw(NULL);
-}
+int alive = 1;
 
 int main(int argc, char* argv[])
 {
@@ -187,7 +70,252 @@ int main(int argc, char* argv[])
 
   if (zm.output_par == NULL)
   {
-    // wm_init(init, update, render, destroy, NULL, 1);
+    srand((unsigned int)time(NULL));
+
+    config_init(); // destroy 1
+    text_ft_init();
+
+    char* path = argv[0];
+
+    char* wrk_path     = cstr_new_path_normalize(path, NULL);                                                                                // REL 0
+    char* cfg_path_loc = zm.cfg_par ? cstr_new_path_normalize(zm.cfg_par, wrk_path) : cstr_new_path_normalize(CFG_PATH_LOC, getenv("HOME")); // REL 1
+    char* cfg_path_glo = cstr_new_cstring(CFG_PATH_GLO);                                                                                     // REL 2
+
+    // print path info to console
+
+    printf("working path  : %s\n", wrk_path);
+    printf("local config path   : %s\n", cfg_path_loc);
+    printf("global config path   : %s\n", cfg_path_glo);
+
+    // read config, it overwrites defaults if exists
+
+    if (config_read(cfg_path_loc) < 0)
+    {
+      if (config_read(cfg_path_glo) < 0)
+        printf("no local or global config file found\n");
+      else
+        printf("using global config\n");
+    }
+    else
+      printf("using local config\n");
+
+    // init font
+
+    char* font_face = config_get("font_face");
+    char* font_path = fontconfig_new_path(font_face ? font_face : ""); // REL 3
+
+    config_set("font_path", font_path);
+
+    // init X11
+
+    int opcode;
+    int event;
+    int error;
+
+    Display* display = XOpenDisplay(NULL);
+
+    if (!XQueryExtension(display, "XInputExtension", &opcode, &event, &error))
+    {
+      printf("X Input extension not available.\n");
+    }
+
+    int blackColor = BlackPixel(display, DefaultScreen(display));
+    int whiteColor = WhitePixel(display, DefaultScreen(display));
+
+    // Create the window
+
+    Window view_win = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 200, 100, 0, whiteColor, blackColor);
+
+    XClassHint hint;
+
+    hint.res_name  = "i3-overview";
+    hint.res_class = "i3-overview";
+
+    XSetClassHint(display, view_win, &hint);
+    XStoreName(display, view_win, "i3-overview");
+
+    // We want to get MapNotify events
+
+    XSelectInput(display, view_win, StructureNotifyMask);
+
+    // Get root window
+
+    Window root_win = DefaultRootWindow(display);
+
+    // Get key events
+
+    XIEventMask mask;
+    mask.deviceid = XIAllDevices;
+    mask.mask_len = XIMaskLen(XI_LASTEVENT);
+    mask.mask     = calloc(mask.mask_len, sizeof(char));
+
+    XISetMask(mask.mask, XI_KeyPress);
+    XISetMask(mask.mask, XI_KeyRelease);
+
+    XISelectEvents(display, root_win, &mask, 1);
+    XSync(display, False);
+
+    free(mask.mask);
+
+    int meta_pressed  = 0;
+    int window_mapped = 0;
+
+    // Event loop
+
+    while (alive)
+    {
+      XEvent               ev;
+      XGenericEventCookie* cookie = (XGenericEventCookie*)&ev.xcookie;
+
+      XNextEvent(display, (XEvent*)&ev);
+
+      if (XGetEventData(display, cookie) &&
+          cookie->type == GenericEvent &&
+          cookie->extension == opcode)
+      {
+
+        XIDeviceEvent* event = cookie->data;
+
+        // printf("    device: %d (%d)\n", event->deviceid, event->sourceid);
+        // printf("    detail: %d\n", event->detail);
+
+        switch (event->evtype)
+        {
+        case XI_KeyPress:
+          if (event->detail == 133 || meta_pressed)
+          {
+            if (event->detail == 133) meta_pressed = 1;
+            // (*l_show)();
+
+            char buff[100] = {0};
+
+            // get i3 workspaces
+
+            FILE* pipe   = popen("i3-msg -t get_workspaces", "r"); // CLOSE 0
+            char* wstree = cstr_new_cstring("{\"items\":");        // REL 0
+
+            while (fgets(buff, sizeof(buff), pipe) != NULL) wstree = cstr_append(wstree, buff);
+
+            wstree = cstr_append(wstree, "}");
+            pclose(pipe); // CLOSE 0
+
+            pipe       = popen("i3-msg -t get_tree", "r"); // CLOSE 0
+            char* tree = cstr_new_cstring("");             // REL 0
+
+            while (fgets(buff, sizeof(buff), pipe) != NULL) tree = cstr_append(tree, buff);
+
+            pclose(pipe); // CLOSE 0
+
+            vec_t* workspaces = VNEW(); // RET 0
+
+            analyzer_extract(wstree, tree, workspaces);
+
+            REL(wstree); // REL 1
+            REL(tree);
+
+            i3_workspace_t* ws  = workspaces->data[0];
+            i3_workspace_t* wsl = workspaces->data[workspaces->length - 1];
+
+            if (ws->width > 0 && ws->height > 0)
+            {
+              int gap  = 25;
+              int cols = 5;
+              int rows = (int)ceilf((float)wsl->number / 5.0);
+
+              int lay_wth = cols * (ws->width / 8) + (cols + 1) * gap;
+              int lay_hth = rows * (ws->height / 8) + (rows + 1) * gap;
+
+              // resize window if necessary
+
+              XWindowAttributes gwa;
+              XGetWindowAttributes(display, view_win, &gwa);
+
+              int wd1 = gwa.width;
+              int ht1 = gwa.height;
+
+              if (wd1 != lay_wth || ht1 != lay_hth)
+              {
+                XResizeWindow(display, view_win, lay_wth, lay_hth);
+
+                int snum   = DefaultScreen(display);
+                int width  = DisplayWidth(display, snum);
+                int height = DisplayHeight(display, snum);
+
+                XMoveWindow(display, view_win, width / 2 - lay_wth / 2, height / 2 - lay_hth / 2);
+              }
+
+              // map window if necessary
+
+              if (!window_mapped)
+              {
+                XMapWindow(display, view_win);
+
+                for (;;)
+                {
+                  XEvent e;
+                  XNextEvent(display, &e);
+                  if (e.type == MapNotify)
+                    break;
+                }
+                window_mapped = 1;
+              }
+
+              // create texture bitmap
+
+              if (bm == NULL || bm->w != lay_wth || bm->h != lay_hth)
+              {
+                bm = bm_new(lay_wth, lay_hth); // REL 0
+              }
+
+              renderer_draw(bm, workspaces);
+
+              XImage* image = XGetImage(display, view_win, 0, 0, lay_wth, lay_hth, AllPlanes, ZPixmap);
+
+              for (int y = 0; y < lay_hth; y++)
+                for (int x = 0; x < lay_wth; x++)
+                {
+                  int      index = y * lay_wth * 4 + x * 4;
+                  uint8_t  r     = bm->data[index];
+                  uint8_t  g     = bm->data[index + 1];
+                  uint8_t  b     = bm->data[index + 2];
+                  uint32_t pixel = (r << 16) | (g << 8) | b;
+                  XPutPixel(image, x, y, pixel);
+                }
+
+              GC gc = XCreateGC(display, view_win, 0, NULL);
+              XPutImage(display, view_win, gc, image, 0, 0, 0, 0, lay_wth, lay_hth);
+            }
+          }
+          break;
+        case XI_KeyRelease:
+          if (event->detail == 133)
+          {
+            //(*l_hide)();
+
+            XUnmapWindow(display, view_win);
+
+            meta_pressed  = 0;
+            window_mapped = 0;
+          }
+          break;
+        }
+      }
+
+      XFreeEventData(display, cookie);
+    }
+
+    XDestroyWindow(display, root_win);
+    XSync(display, False);
+    XCloseDisplay(display);
+
+    // cleanup
+
+    REL(wrk_path);     // REL 0
+    REL(cfg_path_loc); // REL 1
+    REL(cfg_path_glo); // REL 2
+    REL(font_path);    // REL 3
+
+    config_destroy(); // destroy 1
   }
   else
   {
@@ -217,8 +345,7 @@ int main(int argc, char* argv[])
 
       if (bm == NULL || bm->w != lay_wth || bm->h != lay_hth)
       {
-        bm     = bm_new(lay_wth, lay_hth); // REL 0
-        resize = 1;
+        bm = bm_new(lay_wth, lay_hth); // REL 0
       }
 
       renderer_draw(bm, workspaces);
@@ -231,83 +358,10 @@ int main(int argc, char* argv[])
     }
   }
 
-  srand((unsigned int)time(NULL));
-
-  XInitThreads();
-
-  gtk_init(&argc, &argv);
-
-  text_ft_init();
-
-  config_init(); // destroy 1
-
-  char* path = argv[0];
-
-  printf("PATH %s\n", path);
-
-  char* wrk_path     = cstr_new_path_normalize(path, NULL);                                                                                // REL 0
-  char* cfg_path_loc = zm.cfg_par ? cstr_new_path_normalize(zm.cfg_par, wrk_path) : cstr_new_path_normalize(CFG_PATH_LOC, getenv("HOME")); // REL 1
-  char* cfg_path_glo = cstr_new_cstring(CFG_PATH_GLO);                                                                                     // REL 2
-
-  // print path info to console
-
-  printf("working path  : %s\n", wrk_path);
-  printf("local config path   : %s\n", cfg_path_loc);
-  printf("global config path   : %s\n", cfg_path_glo);
-
-  // read config, it overwrites defaults if exists
-
-  if (config_read(cfg_path_loc) < 0)
-  {
-    if (config_read(cfg_path_glo) < 0)
-      printf("no local or global config file found\n");
-    else
-      printf("using global config\n");
-  }
-  else
-    printf("using local config\n");
-
-  char* font_face = config_get("font_face");
-  char* font_path = fontconfig_new_path(font_face ? font_face : ""); // REL 3
-
-  printf("font_path %s\n", font_path);
-
-  config_set("font_path", font_path);
-
-  // main loop
-
-  // Read a raw image data from the disk and put it in the buffer.
-  // ....
-
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  image  = gtk_image_new();
-
-  listener_start(show, hide); // destroy 2
-
-  gtk_window_set_title(GTK_WINDOW(window), "Image Viewer");
-
-  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-  gtk_container_add(GTK_CONTAINER(window), image);
-
-  gtk_widget_show_all(window);
-  gtk_widget_hide(window);
-
-  gtk_main();
-
-  g_object_unref(window);
-  g_object_unref(image);
-
-  // cleanup
-
-  REL(wrk_path);     // REL 0
-  REL(cfg_path_loc); // REL 1
-  REL(cfg_path_glo); // REL 2
-  REL(font_path);    // REL 3
-
-  config_destroy(); // destroy 1
-
-  if (zm.cfg_par) REL(zm.cfg_par); // REL 0
+  if (zm.cfg_par) REL(zm.cfg_par);             // REL 0
+  if (zm.ws_json_par) REL(zm.ws_json_par);     // REL 0
+  if (zm.tree_json_par) REL(zm.tree_json_par); // REL 0
+  if (zm.output_par) REL(zm.output_par);       // REL 0
 
 #ifdef DEBUG
   mem_stats();
